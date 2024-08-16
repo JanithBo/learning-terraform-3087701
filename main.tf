2f9a14d92450dc284cfe8851145d423798ba8a3f
Janith Bojitha <72045158+JanithBo@users.noreply.github.com>
2024-08-16 16:47:00 +0530

    create new dev vpc

--- main.tf
+++ main.tf
@@ -17,20 +17,17 @@ data "aws_vpc" "default" {
   default = true
 }
 
+data "aws_vpc" "default"{
+  default = true
+}
 
 module "blog_vpc" {
-  source = "terraform-aws-modules/vpc/aws"
-
-  name = "dev"
-  cidr = "10.0.0.0/16"
-
-  azs             = ["us-west-2a","us-west-2b","us-west-2c"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
-
-  tags = {
-    Terraform = "true"
-    Environment = "dev"
-  }
+  source                                     = "dwp/vpc/aws"
+  vpc_name                                   = "dev"
+  region                                     = "us-west-2"
+  vpc_cidr_block                             = "192.168.0.0/24"
+  interface_vpce_source_security_group_ids   = ["${module_blog_sg.aws_security_group.source.id}"]
+  interface_vpce_subnet_ids                  = ["${module_blog_vpc.public_subnets[0]}"]
 }
 
 
@@ -87,7 +84,7 @@ module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "4.13.0"
 
-  vpc_id  = data.aws_vpc.default.id
+  vpc_id  = module.blog_vpc.vpc_id
   name    = "blog"
   ingress_rules = ["https-443-tcp","http-80-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
